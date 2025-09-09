9baad201a01872350942fbf7783c92e2d277ecc6
Giskekommune <dagny.kristin.rodland@giske.kommune.no>
2025-09-09 14:46:16 +0200

    Add files via upload

---
+++ data/Kulturminner_1532_Giske_25832_Trehusmiljoer_GMLTrehusmilj_9.js
@@ -0,0 +1,1 @@
+var json_Kulturminner_1532_Giske_25832_Trehusmiljoer_GMLTrehusmilj_9 = {"type":"FeatureCollection","name":"Kulturminner_1532_Giske_25832_Trehusmiljoer_GMLTrehusmilj_9","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
